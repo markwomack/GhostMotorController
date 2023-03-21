55590d2c9d862c03234f0c4562122f608b55663a
Mark Womack <mwomack@gmail.com>
2023-03-21 12:17:12 -0400

    Comment cleanups

--- Tests/DriveByRC/DriveByRC.ino
+++ Tests/DriveByRC/DriveByRC.ino
@@ -10,11 +10,11 @@
 #include <Arduino.h>
 
 // Third Party includes
-#include <DebugMsgs.h>
-#include <TaskManager.h>
+#include <DebugMsgs.h>   // https://github.com/markwomack/ArduinoLogging
+#include <TaskManager.h> // https://github.com/markwomack/TaskManager
 #include <BlinkTask.h>
-#include <MotorAndEncoderManager.h>
-#include <MotorController.h>
+#include <MotorAndEncoderManager.h>  // https://github.com/markwomack/MotorAndEncoderManager
+#include <MotorController.h>         // https://github.com/markwomack/MotorAndEncoderManager
 #include <GhostMotorManager.h>
 #include <ThreePhaseMotorEncoder.h>
 
@@ -30,6 +30,8 @@ MotorController* motorController;
 
 BlinkTask idleTask;
 
+// A task to set the desired motor speeds based on input
+// from two RC signals.
 class SetSpeedFromRCTask : public Task {
   public:
     void setRCPins(uint8_t chan1Pin, uint8_t chan2Pin) {
@@ -49,14 +51,18 @@ class SetSpeedFromRCTask : public Task {
       digitalWrite(M1_BRAKE_PIN, HIGH);
       motorController->stop();
     };
-    
-    int readChannel(int channelPin, int minLimit, int maxLimit, int defaultValue) {
+
+    // Reads the current RC signal on the given pin, returns it
+    // as a value between minLimit and maxLimit. Returns NO_RC_SIGNAL
+    // if there is no RC signal.
+    int readChannel(int channelPin, int minLimit, int maxLimit) {
       int ch = pulseIn(channelPin, HIGH, 30000);
-      if (ch < 100) return defaultValue;
+      if (ch < 100) return NO_RC_SIGNAL;
       int value = map(ch, 1020, 1980, minLimit, maxLimit);
       return min(max(minLimit, value), maxLimit);
     }
-    
+
+    // Performs a controlled stop on both motors
     void performControlledStop(void) {
       motorController->setDesiredSpeeds(0, 0);
       uint32_t checkTime = 0;
@@ -76,12 +82,17 @@ class SetSpeedFromRCTask : public Task {
       }
     };
     
+    // Called periodically to set the desired moto speeds from the
+    // the RC signals.
     void update(void) {
-      int ch1Val = readChannel(_chan1Pin, -100, 100, NO_RC_SIGNAL);
-      int ch2Val = readChannel(_chan2Pin, -100, 100, NO_RC_SIGNAL);
+
+      // Read the current RC signals on both channels
+      int ch1Val = readChannel(_chan1Pin, -100, 100);
+      int ch2Val = readChannel(_chan2Pin, -100, 100);
 
       DebugMsgs.debug().print("RC Channels: ").print(ch1Val).print(" : ").println(ch2Val);
-      
+
+      // If either channel has been lost, then stop the robot, stop all the tasks.
       if (ch1Val == NO_RC_SIGNAL || ch2Val == NO_RC_SIGNAL) {
         DebugMsgs.debug().println("RC channel lost, stopping...");
         performControlledStop();
@@ -90,21 +101,27 @@ class SetSpeedFromRCTask : public Task {
       }
 
       // maintain a value of zero if close to zero
+      // avoids shimming
       if (abs(ch1Val) <= 5) {
         ch1Val = 0;
       }
       if (abs(ch2Val) <= 5) {
         ch2Val = 0;
       }
-      
+
+      // Channel 2 is for linear (forward/reverse) velocity
       double linearVelocity = ((double)ch2Val/100.0) * MAX_LINEAR_VELOCITY;
+
+      // Channel 1 is for angular (left/right) velocity
       double angularVelocity = ((double)ch1Val/100.0) * MAX_ANGULAR_VELOCITY;
-      
+
+      // Calculate the motor speeds to match
       double m0Speed = linearVelocity - angularVelocity;
       double m1Speed = linearVelocity + angularVelocity;
       
       DebugMsgs.debug().print("Setting speeds: ").print(m0Speed).print(" : ").println(m1Speed);
-      
+
+      // Set the desired speeds on the motor controller
       motorController->setDesiredSpeeds(m0Speed, m1Speed);
     };
 
@@ -114,6 +131,8 @@ class SetSpeedFromRCTask : public Task {
 };
 SetSpeedFromRCTask setSpeedFromRCTask;
 
+// Task to allow the motor controller to periodically adjust
+// the motor speeds to match the current desired speeds
 class AdjustSpeedsTask : public Task {
   public:
     void update(void) {
@@ -124,7 +143,8 @@ AdjustSpeedsTask adjustSpeedsTask;
 
 void setup() {
   Serial.begin(9600);
-  
+
+  // Comment out to disable debug output
   DebugMsgs.enableLevel(DEBUG);
   
   // RC Channel inputs
@@ -149,15 +169,19 @@ void setup() {
   DebugMsgs.debug().print("MAX_LINEAR_VELOCITY: ").println(MAX_LINEAR_VELOCITY);
   DebugMsgs.debug().print("MAX_ANGULAR_VELOCITY: ").println(MAX_ANGULAR_VELOCITY);
 
+  // Create the motor controller
   motorController = new MotorController(motorManager, KP, KI, KD, 50, RADIANS_PER_TICK, MAX_RADIANS_PER_SECOND);
 
+  // Set the RC pins into the setSpeedFromRCTask
   setSpeedFromRCTask.setRCPins(RC_CH1_PIN, RC_CH2_PIN);
-  
+
+  // Set up the task manager with tasks
   taskManager.addIdleTask(&idleTask, 100);
   taskManager.addBlinkTask(500);
   taskManager.addTask(&setSpeedFromRCTask, 50);
   taskManager.addTask(&adjustSpeedsTask, 10);
-  
+
+  // Wait for the user to press the button to start
   taskManager.startMonitoringButton(BUTTON_PIN, HIGH);
 }
 
--- Tests/DriveByRC/globals.h
+++ Tests/DriveByRC/globals.h
@@ -6,23 +6,36 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
+// Constants used with PID controller in the motor controller
+const double KP(0.25);
+const double KI(0.0008);
+const double KD(0.0005);
+
+const int NO_RC_SIGNAL(-10000);
+
 //*** These are the contants based on the specific motors and encoders being used.
-// You should use values that match your own robot.
+//*** You should use values that match your own robot.
 
 // Total encoder ticks for single wheel revolution
 const double TICKS_PER_ROTATION(120.0); // 120 ticks for BLDC motor
 
+// This constant is based on measurements of the motor performance using
+// full motor power your robot is using. Your mileage may vary and you should
+// verify your robots values by running the motors at full power (1.0) for a
+// second and recording the encoder values. Or running the motors at full
+// poer for an extended time and taking the average ticks per second.
+// When motor is run at full for one second, this is approximately how
+// many ticks will be counted.
+const double MAX_TICKS_PER_SECOND(925.0);
+
+// Wheel diameter in meters (12 inches)
+const double WHEEL_DIAM_M(0.3048);
+
+// *** Everything below here is calculated using the three values above
+
 // Code will use Radians (angular velocity) for velocity measurements
 // Note: There are 2 radians in a single rotation (360 degrees)
 const double RADIANS_PER_TICK(2 / TICKS_PER_ROTATION);
-
-// This constant is based on measurements of the motor performance using
-// full motor power your robot is using (This one used 5v to the motors).
-// Your mileage may vary and you should verify your robots values by running
-// the motors at full power (1.0) for a second and recording the encoder values.
-// When motor is run at full for one second, this is approximately
-// how many ticks will be counted.
-const double MAX_TICKS_PER_SECOND(925.0);
 
 // This is the number of wheel rotations when run at full for one second.
 const double MAX_ROTATIONS_PER_SECOND(MAX_TICKS_PER_SECOND / TICKS_PER_ROTATION);
@@ -33,19 +46,13 @@ const double MAX_RADIANS_PER_SECOND(MAX_ROTATIONS_PER_SECOND * 2);
 
 // Just for reference, this the max meters per second when run at full speed.
 // It is just for reference, since the MotorController will use radians/second.
-// Wheel diameter in meters (12 inches)
-const double WHEEL_DIAM_M(0.3048);
 const double WHEEL_CIRCUMFERENCE_M(PI * WHEEL_DIAM_M); // C=2*pi*r -> C=pi*2r -> C=pi*D
 const double MAX_METERS_PER_SECOND(MAX_ROTATIONS_PER_SECOND * WHEEL_CIRCUMFERENCE_M);
 
+// Maximum velocity allowed for linear velocity (forward/reverse)
 const double MAX_LINEAR_VELOCITY(0.2 * MAX_RADIANS_PER_SECOND);  // 20% of maximum speed
+
+// Maximum velocity allowed for angular velocity (left/right)
 const double MAX_ANGULAR_VELOCITY(0.1 * MAX_RADIANS_PER_SECOND); // 10% of maximum speed
-
-// Constants used with PID controller in the motor controller
-const double KP(0.25);
-const double KI(0.0008);
-const double KD(0.0005);
-
-const int NO_RC_SIGNAL(-10000);
 
 #endif // GLOBALS_H
